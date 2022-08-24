1739d1dc4ac6893539692b4b2d039c1ef17d3a7c
Omer Ozekin <ozekinomer@gmail.com>
2022-08-24 18:48:14 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            // 1 den başlayarak console dan girilen sayıya kadar ortalama hesaplayıp console'a yazdıran program.
+
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+                
+            }
+            Console.WriteLine(toplam/sayi);
+
+            char character = 'a';
+
+            while (character<'z')
+            {
+                Console.Write(character);
+                character++;
+                
+            }
+
+            //Foreach
+            string[] arabalar = {"BMW","Ford","Toyota","Volvo"}
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+                
+            }
+
+        }
+
+
+    }
+}
